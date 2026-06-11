a4c4f2c9d2faa9182411aeeadd2d03ff82bf04d7
sahuishan01 <ishan20015.s@gmail.com>
2026-06-11 21:36:38 +0530

    fix: add kotlinx.coroutines.launch import

--- android/app/src/main/java/com/khata/app/ui/navigation/KhataNavHost.kt
+++ android/app/src/main/java/com/khata/app/ui/navigation/KhataNavHost.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import kotlinx.coroutines.launch
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.NavHost
