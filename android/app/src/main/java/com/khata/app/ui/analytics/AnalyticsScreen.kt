f8ae455fb015f7d7efe1a880c4d052c0197ae49e
sahuishan01 <ishan20015.s@gmail.com>
2026-06-12 09:33:39 +0530

    fix: add background import to AnalyticsScreen

--- android/app/src/main/java/com/khata/app/ui/analytics/AnalyticsScreen.kt
+++ android/app/src/main/java/com/khata/app/ui/analytics/AnalyticsScreen.kt
@@ -1,5 +1,6 @@
 package com.khata.app.ui.analytics
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
